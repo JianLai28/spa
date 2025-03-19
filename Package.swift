e4246718cdd268c29011200f569b24709b505495
JianLai <62128621+JianLai28@users.noreply.github.com>
2025-03-19 18:45:11 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .target(
             name: "AccountManager",
-            dependencies: [],
-            
+            dependencies: []
         )
     ]
 )
